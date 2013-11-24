459eaad090008f4f4da87ba2917da60d3b095150
U-lloydzhou\lloyd <lloydzhou@qq.com>
2013-11-24 12:10:41 +0800

    update model

--- test/index.php
+++ test/index.php
@@ -13,7 +13,7 @@ class Expressions extends Base {
 	}
 }
 class WrapExpressions extends Expressions {
-	public $delimiter = ' ';
+	public $delimiter = ', ';
 	public $startWrapper = '(';
 	public $endWrapper = ')';
 	public function __toString() {
@@ -52,50 +52,69 @@ class Field extends Base {
 				'target'=>(is_array($args[0]) ? new WrapExpressions(array('startWrapper' =>"('", 'endWrapper' =>"')", 'delimiter' => "', '",'target' => $args[0])) : $args[0])));
 	}
 }
-class BaseTable extends Base {
-	protected $expressions = array();
-	protected $warpOpen = false;
-	public $where;
-	public $select;
-	public $limit;
-	public $order;
-	public $from;
+class Table extends Base //implements Iterator
+{
+	protected $sqlExpressions = array('expressions' => array(), 'wrapOPen' => false,
+		'select'=>null, 'insert'=>null, 'update'=>' ', 'delete'=>' ', 
+		'from'=>null, 'values' => null, 'where'=>null, 'limit'=>null, 'order'=>null);
 	protected $fields = array();
 	public $table;
 	public $data = array();
 	
-	public function find() {
+	public static $driver=null;
+	public static function config($driver) {
+		self::$driver = $driver;
+	}
+	public function __set($name, $val){
+		if (array_key_exists($name, $this->sqlExpressions)) $this->sqlExpressions[$name] = $val;
+		if (array_key_exists($name, $this->sqlExpressions)) $this->data[$name] = $val;
+	}
+	protected function find() {
 		$this->limit = '1';
-		$this->getdata($this->getSql());
-		return current($this->data);
+		$this->data = current(self::$driver->getdata($this->getSql(array('select', 'from', 'where', 'limit', 'order'))));
 	}
-	public function findAll() {
-		return $this->getdata($this->getSql());
+	public function findAll($resetLimit = false) {
+		if ($resetLimit) $this->limit = null;
+		$this->data = self::$driver->getdata($this->getSql(array('select', 'from', 'where', 'limit', 'order')));
+		array_walk($this->data, function (&$n, $i, $o){
+			$n = Table::forTable($o->table, array('data' => $n));
+		}, $this);
+		return $this->data;
 	}
-	public function getSql() {
-		$sqls = array('select', 'insert', 'update', 'delete', 'from', 'where', 'limit', 'order');
+	protected function insert($data = null) {
+		$data = $data ? $data : $this->data;
+		$this->insert = new Expressions(array('operator' => 'into '. $this->table, 'target' => new WrapExpressions(array('delimiter' => ', ', 'target' => array_keys($data)))));
+		$this->values = new WrapExpressions(array('delimiter' => ', ', 'target' => array_values($data)));
+		self::$driver->getdata($this->getSql(array('insert', 'values')));
+		//$this->data
+	}
+	public function getSql($sqls = array()) {
 		array_walk($sqls, function (&$n, $i, $o){$n = (null !== $o->$n) ? strtoupper($n).' '. $o->$n. ' ' : '';}, $this);
+		echo 'SQL: '. implode('', $sqls). "\n";
 		return implode('', $sqls);		
 	}
 	public function __get($name) {
 		if (isset($this->fields[$name])) return $this->fields[$name];
+		if (array_key_exists($name, $this->sqlExpressions)) return $this->sqlExpressions[$name];
 	}
-	// abstract function.
 	public function __call($name, $args) {
-		if (method_exists($this, $name)) {
-			call_user_func_array(array($this, $name), $args);
-			return $this;
+		if (isset(self::$driver->$name)) {
+			echo 'call from driver';
+			return call_user_func_array(array(self::$driver, $name), $args);
 		}
-		$name = strtolower($name);
-		$field = $args[0];
-		$operator = (is_string(end($args)) && 'or' === strtolower(end($args))) ? 'OR' : 'AND';
-		if (isset($this->fields[$field])){
-			array_shift($args) && ($exp = call_user_func_array(array($this->fields[$field], $name), $args));
-			if ($exp){ 
-				if (!$this->wrapOpen)
-					$this->_addCondition($exp, $operator);
-				else
-					$this->_addExpression($exp, $operator);
+		if (method_exists($this, $name)) 
+			call_user_func_array(array($this, $name), $args);
+		elseif (isset($args[0]) && $field = $args[0]) {
+			$name = strtolower($name);
+			$operator = (is_string(end($args)) && 'or' === strtolower(end($args))) ? 'OR' : 'AND';
+			if (isset($this->fields[$field])){
+				array_shift($args) && ($exp = call_user_func_array(array($this->fields[$field], $name), $args));
+				if ($exp){ 
+					if (!$this->wrapOpen)
+						$this->_addCondition($exp, $operator);
+					else
+						$this->_addExpression($exp, $operator);
+				}
 			}
 		}
 		return $this;
@@ -126,63 +145,40 @@ class BaseTable extends Base {
 		else 
 			$this->where = new Expressions(array('source'=>$this->where, 'operator'=>$operator, 'target'=>$exp));	
 	}
-	public static function forTable($table, $config = array(), $class = 'PDOTable') {
-		$instance = new $class (array_merge($config, array('table' => $table, 'from' => $table)));
-		foreach($instance->getdata('SHOW FULL COLUMNS FROM '. $table) as $column)
-		$instance->addField($column['Field'], array('name' => $column['Field'], 'type' => $column['Type']));
+	public static function forTable($table, $config = array()) {
+		$instance = new Table (array_merge($config, array('table' => $table, 'from' => $table)));
+		foreach(self::$driver->getdata('SHOW FULL COLUMNS FROM '. $table) as $column)
+			$instance->addField($column['Field'], array('name' => $column['Field'], 'type' => $column['Type']));
 		return $instance;
 	}
-	
-	public function rewind() {return $this->data->rewind();}
-	public function current() {return $this->data->current();}
-	public function key() {return $this->data->key();}
-	public function next() {return $this->data->next();}
-	public function valid() {return $this->data->valid();}
-	
 }
-class PDOTable extends BaseTable implements Iterator {
-	public static $db = null;
+class PDODriver extends Base{
+	public $db = null;
 	public $sth = null;
 		
-	public static function config($dsn, $user = '', $password = '') {
-		self::$db = new PDO($dsn, $user, $password);
-	}
 	public function __call($name, $args) {
-		if (method_exists(self::$db, $name)) return $this->sth = call_user_func_array(array(self::$db, $name), $args);
+		if (method_exists($this->db, $name)) return $this->sth = call_user_func_array(array($this->db, $name), $args);
 		if (method_exists($this->sth, $name)) return call_user_func_array(array($this->sth, $name), $args);
-		return parent::__call($name, $args);
+	}
+	public function __isset($name) {
+		return (method_exists($this->db, $name) || method_exists($this->sth, $name));
+	}
+	public function execsql($sql) {
+		
 	}
 	public function getdata($sql) {
-		$this->sth = $this->prepare($sql);
+		$this->sth = $this->db->prepare($sql);
 		$this->sth->execute();
-		return $this->data = $this->sth->fetchAll(PDO::FETCH_ASSOC);
+		$data = $this->sth->fetchAll(PDO::FETCH_ASSOC);
+		$this->sth = null;
+		return $data;
 	}
 }
-// PRAGMA table_info(tbl)
-// SHOW FULL COLUMNS FROM tbl
-// 
-//$db = new PDO('mysql:host=localhost;dbname=test');
-//$stmt = $db->prepare("select * from tbl limit 0");
-
-
-/*
-$e1 = new Expressions(array('source'=>'name', 'operator'=>'=', 'target'=>"'asdaasda'"));
-$e2 = new Expressions(array('source'=>'email', 'operator'=>'=', 'target'=>"'asdaasda@sina.com'"));
-$e3 = new Expressions(array('source'=>$e1, 'operator'=>'and', 'target'=>$e2));
-$e4 = new Expressions(array('source'=>$e1, 'operator'=>'and', 'target'=>$e2));
-$e = new Expressions(array('source'=>$e3, 'operator'=>'or', 'target'=>$e4));
-echo $e, "\n";
-
-$t = new PDOTable();
-$t->addField('name');
-$t->addField('id');
-echo $t->wrap()->isNull('name')->eq('name', "'lloyd'")->ne('name', "'lloyd'")->wrap(true)->wrap()->in('id', array(1,3,4,2,2))->notNull('id')->wrap('OR')->condition, "\n";
-//var_dump($t);
- */
-PDOTable::config('mysql:host=localhost;dbname=test');
-//var_dump(PDOTable::$db);
-$tbl = PDOTable::forTable('tbl');
-echo $tbl->isnotnull()->wrap()->ge('id', 1)->wrap('OR')->from, "\n";
-//var_dump($tbl);
+Table::config(new PDODriver(array('db' => new PDO('mysql:host=localhost;dbname=test'))));
+$tbl = Table::forTable('tbl');
+echo $tbl->isnotnull('id')->wrap()->ge('id', 1)->wrap('OR')->where, "\n";
 echo $tbl->select('id, col')->select, "\n";
-var_dump($tbl->findAll());
+$tbl->order = 'by id asc';
+var_dump($tbl->find());
+$tbl->insert(array('id' => 12, 'col' => 100));
+foreach($tbl->findAll(true) as $t) var_dump($t);
